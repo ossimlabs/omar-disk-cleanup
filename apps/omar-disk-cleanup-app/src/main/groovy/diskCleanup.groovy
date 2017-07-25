aea608c94c8742b4f73beceef992d11cb75e6b8b
knaquin <kevin.naquin@digitalglobe.com>
2017-07-25 11:54:44 -0400

    Testing...

--- apps/omar-disk-cleanup-app/src/main/groovy/diskCleanup.groovy
+++ apps/omar-disk-cleanup-app/src/main/groovy/diskCleanup.groovy
@@ -23,42 +23,43 @@ println "Used Disk Space: ${ convertBytesToHumanReadable( usedDiskSpace ) }"
 println "Current disk usage: ${ (usedDiskSpace / totalDiskSpace * 100 as Double).trunc( 2 ) } %"
 
 if (usedDiskSpace > totalDiskSpace * maxDiskLimit) {
-  println "The maximum disk limit has been exceeded!"
-  def numberOfBytesToDelete = (totalDiskSpace - freeDiskSpace) - minDiskLimit * totalDiskSpace
-  println "I will try and delete approx. ${ convertBytesToHumanReadable( numberOfBytesToDelete ) } of data..."
+    println "The maximum disk limit has been exceeded!"
+    def numberOfBytesToDelete = (totalDiskSpace - freeDiskSpace) - minDiskLimit * totalDiskSpace
+    println "I will try and delete approx. ${ convertBytesToHumanReadable( numberOfBytesToDelete ) } of data..."
 
-  def sql = Sql.newInstance( jdbcUrl, username, password, "org.postgresql.Driver" )
-  def sqlCommand = "SELECT filename FROM raster_entry ORDER BY ingest_date ASC;"
-  sql.eachRow( sqlCommand ) {
-    def filename = it.filename
+    def sql = Sql.newInstance( jdbcUrl, username, password, "org.postgresql.Driver" )
+    def sqlCommand = "SELECT filename FROM raster_entry ORDER BY ingest_date ASC;"
+    sql.eachRow( sqlCommand ) {
+        def filename = it.filename
 
-    println "Deleting all files associated with ${ filename }..."
-    def http = new HTTPBuilder( "${ removeRasterUrl }?deleteFiles=true&filename=${ filename }" )
-    http.request( POST ) { req ->
-      response.failure = { resp, reader -> println "Failure: ${ reader }" }
-      response.success = { resp, reader -> println "Success: ${ reader }" }
+        println "Deleting all files associated with ${ filename }..."
+        def http = new HTTPBuilder( "${ removeRasterUrl }?deleteFiles=true&filename=${ filename }" )
+        //http.request( POST ) { req ->
+        //    response.failure = { resp, reader -> println "Failure: ${ reader }" }
+        //    response.success = { resp, reader -> println "Success: ${ reader }" }
+        //}
+        println http.properties
+
+        def file = new File( filename )
+        if ( file.exists() ) {
+            println "Uh oh! I couldn't delete ${ filename }, so I'm going to stop."
+            sql.close()
+            System.exit( 1 )
+        }
+
+        def usedDiskSpacePercentage = (totalDiskSpace - new File( diskVolume ).getUsableSpace()) / totalDiskSpace as Double
+        println "Disk space being used: ${ (usedDiskSpacePercentage * 100).trunc( 2 ) } %"
+
+        if (usedDiskSpacePercentage < minDiskLimit) {
+            println "I successfully deleted enough data!"
+            sql.close()
+            System.exit( 0 )
+        }
     }
 
-    def file = new File( filename )
-    if ( file.exists() ) {
-      println "Uh oh! I couldn't delete ${ filename }, so I'm going to stop."
-      sql.close()
-      System.exit( 1 )
-    }
-
-    def usedDiskSpacePercentage = (totalDiskSpace - new File( diskVolume ).getUsableSpace()) / totalDiskSpace as Double
-    println "Disk space being used: ${ (usedDiskSpacePercentage * 100).trunc( 2 ) } %"
-
-    if (usedDiskSpacePercentage < minDiskLimit) {
-      println "I successfully deleted enough data!"
-      sql.close()
-      System.exit( 0 )
-    }
-  }
-
-  println "Well, I deleted everything I could but it doesn't look like it was enough!"
-  sql.close()
-  System.exit( 1 )
+    println "Well, I deleted everything I could but it doesn't look like it was enough!"
+    sql.close()
+    System.exit( 1 )
 }
 
 
