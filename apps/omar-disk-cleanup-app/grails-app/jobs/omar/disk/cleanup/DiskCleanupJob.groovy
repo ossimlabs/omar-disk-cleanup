291c9556a94b10ad64890e8366bc2fe84aa0d4d2
knaquin <kevin.naquin@digitalglobe.com>
2018-10-01 12:10:59 -0400

    Testing...

--- apps/omar-disk-cleanup-app/grails-app/jobs/omar/disk/cleanup/DiskCleanupJob.groovy
+++ apps/omar-disk-cleanup-app/grails-app/jobs/omar/disk/cleanup/DiskCleanupJob.groovy
@@ -1,13 +1,8 @@
 package omar.disk.cleanup
 
-
-import groovyx.net.http.HTTPBuilder
-import static groovyx.net.http.Method.POST
-
-
 class DiskCleanupJob {
 
-    def diskCleanupService
+    //def diskCleanupService
 
     static triggers = {
         simple repeatInterval: 600000 // execute job every 10 minutes
@@ -15,6 +10,6 @@ class DiskCleanupJob {
 
 
     def execute() {
-        diskCleanupService.cleanup()
+        //diskCleanupService.cleanup()
     }
 }
