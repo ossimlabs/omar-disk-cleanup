3350044258539adf212e8650d11ab89057f6040c
knaquin <kevin.naquin@digitalglobe.com>
2018-10-01 15:38:44 -0400

    Testing...

--- apps/omar-disk-cleanup-app/grails-app/services/omar/disk/cleanup/DiskCleanupService.groovy
+++ apps/omar-disk-cleanup-app/grails-app/services/omar/disk/cleanup/DiskCleanupService.groovy
@@ -102,7 +102,7 @@ class DiskCleanupService {
     }
 
     def removeStaleFiles() {
-        RasterEntry.each {
+        RasterEntry.list().each {
             println it.properties
         }
         //def filenames = RasterEntry.list( sort: "ingest_date", order: "asc" ).collect({ it.filename })
